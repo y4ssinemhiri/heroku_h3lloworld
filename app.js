c11557c5e78a15770adfffa507a077215408ec01
Yassine <yassine@MacBook-Pro-de-Yassine.local>
2020-04-19 01:06:10 +0200

    fb tutorial quick start code

--- app.js
+++ app.js
@@ -6,3 +6,38 @@ app.get('/webhook', (req, res) => {
   
   
 })
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+}
