27e555544e5872103f63978e934db1735df76a86
Yassine <yassine@MacBook-Pro-de-Yassine.local>
2020-04-19 12:56:32 +0200

    back to working state

--- index.js
+++ index.js
@@ -29,14 +29,6 @@ app.post('/webhook', (req, res) => {
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
 
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-   	 handleMessage(sender_psid, webhook_event.message);        
-      }
-      else if (webhook_event.postback) {
-   	 handlePostback(sender_psid, webhook_event.postback);
-      }  
   });
 
     // Returns a '200 OK' response to all requests
@@ -77,21 +69,3 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACESS_TOKEN;
-
-app.get('/webhook', (req, res) => {
-
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
-function handleMessage(sender_psid, received_message) {
-
-  let response;
-
-  // Check if the message contains text
-  if (received_message.text) {
-
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an$
-    }
-  }
