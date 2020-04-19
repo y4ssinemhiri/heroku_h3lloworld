30881ec5a573d9e18aa7420aba6b2a928113f50f
Yassine <yassine@MacBook-Pro-de-Yassine.local>
2020-04-19 13:25:19 +0200

    update index.js

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ app.post('/webhook', (req, res) => {
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
+     
+     // Check if the event is a message or postback and
+     // pass the event to the appropriate handler function
+     if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      }
+     else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+     }
 
   });
 
