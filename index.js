36ad9ffe0730a8e1b9750caf18133c385d20d19b
Yassine <yassine@MacBook-Pro-de-Yassine.local>
2020-04-19 13:45:07 +0200

    update index.js

--- index.js
+++ index.js
@@ -80,7 +80,20 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+  
+    let response;
 
+    // Check if the message contains text
+    if (received_message.text) {    
+
+    // Create the payload for a basic text message
+        response = {
+          "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+     }  
+  
+     // Sends the response message
+     callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
@@ -90,5 +103,11 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+   // Construct the message body
+    let request_body = {
+       "recipient": {
+       "id": sender_psid
+      },
+      "message": response
+    }
 }
